55a47618f3ee560581fefbf5b8de94ba6125871c
enzodev-design <enzodiego43@gmail.com>
2026-02-27 11:12:23 -0300

    pratica um e dois do curso de java

---
+++ PraticaDois.java
@@ -0,0 +1,69 @@
+public class PraticaDois {
+    public static void main(String[] args) {
+
+        String nome;
+        double primeiraNota;
+        double segundaNota;
+        double a;
+        int b;
+
+
+
+        nome = "joao";
+        primeiraNota = 10;
+        segundaNota = 7;
+        System.out.println("A MEDIAFINAL DE : " + nome + " e de " + ((primeiraNota + segundaNota) / 2));
+
+
+        nome = "ana";
+        primeiraNota = 5;
+        segundaNota = 7;
+        System.out.println("A MEDIAFINAL DE : " + nome + " e de " + ((primeiraNota + segundaNota) / 2));
+
+        nome = "renata";
+        primeiraNota = 5;
+        segundaNota = 8;
+        System.out.println("A MEDIAFINAL DE : " + nome + " e de " + ((primeiraNota + segundaNota) / 2));
+        boolean c = (primeiraNota == segundaNota);
+        System.out.println(c);
+
+        b= 10;
+        a=15;
+         if (b > a ){
+            System.out.println(" numero aceito ");}
+         else{
+                System.out.println("numero nao alocado");
+        }
+         b=10;
+         a=50.5;
+
+        double total = ((int) a + b);
+        System.out.println(total);
+
+        char letra;
+        String palavra;
+
+        palavra= "eu estudei na escola xenofonte na  turma ";
+        letra= 'a';
+        System.out.println(palavra + letra );
+
+        double precoProduto;
+        int quantidade;
+
+         precoProduto = 5.99;
+         quantidade= 5;
+
+        System.out.println( "O preco do produto ficou em : "+ ((int)precoProduto * quantidade)+ " reais ao todo.");
+
+        double valorEmDolares =4.94;
+        double reais;
+
+        System.out.println(" o valor de 3 dolares em reias ficou em : " + (valorEmDolares *(reais= 3))+ " reais.");
+
+
+
+
+
+
+    }
+}
---
+++ PraticaUm.java
@@ -0,0 +1,37 @@
+public class PraticaUm {
+    public static void main(String[] args) {
+
+        /* ATRIBUTOS
+        int primeiroNumero = 5;
+        int segundoNumero = 10;
+
+          CONSTRUTOR
+        int total = primeiroNumero + segundoNumero;
+        System.out.println(total);}*/
+
+        //atributos
+        String nome = "enzo";
+        int idade = 20;
+        int numeroUm = 10;
+        int numeroDois= 5;
+
+        //construtor
+
+       System.out.println("Olá " + nome + " como voce esta ?");
+        System.out.println("estou bem ");
+        System.out.println("qual a sua idade?");
+        System.out.println("minha idade é "+ idade );
+        System.out.println("numero da minha casa e" + numeroUm );
+
+       // int total = numeroUm + numeroDois;
+        System.out.println( " a soma dos ois numero  e igual : " +( numeroUm + numeroDois));
+        System.out.println(" a subitracao ods numeros e igual : " + (numeroUm - numeroDois));
+        System.out.println("a multiplicao dos numeros e igual : "+ (numeroUm * numeroDois));
+        System.out.println("a divisao dos numeros e igual a " + (numeroUm / numeroDois));
+
+        System.out.println( "meu nome e " + nome + " minha idae e " + idade + " o numero da minha casa e " + numeroDois );
+
+
+    }
+}
+
